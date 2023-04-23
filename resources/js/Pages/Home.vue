29a9e7005d4bcd569d84da52626306ea9d9c006b
coltrida <coltrida@gmail.com>
2023-04-23 20:36:23 +0200

    si procede con la barra di naviazione con il controllo del login

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,29 +1,22 @@
 <template>
     <h2 class="text-red-600">Home</h2>
-    <div v-for="user in getUsers" :key="user.id">
-        {{user.name}}
-    </div>
 </template>
 
 <script>
-import {mapActions, mapGetters} from "vuex";
+
 export default {
     name: "Home",
 
     mounted() {
-        this.fetchUsers();
+
     },
 
     methods:{
-        ...mapActions('users', {
-            fetchUsers: 'fetchUsers',
-        }),
+
     },
 
     computed:{
-        ...mapGetters('users', {
-            getUsers: 'getUsers',
-        }),
+
     }
 }
 </script>
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,20 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
 
-Route::get('/users', [UserController::class, 'index']);
+
+
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/users', [UserController::class, 'index']);
+});
